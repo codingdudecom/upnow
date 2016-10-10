464143ecdaaddb75dd94eefe1c1308f67c6cfdd0
John Negoita <johnny.php@gmail.com>
2016-10-10 09:24:55 +0300

    models

---
+++ api/controllers/UserGroupController.js
@@ -0,0 +1,11 @@
+/**
+ * UserGroupController
+ *
+ * @description :: Server-side logic for managing usergroups
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
