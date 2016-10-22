ccb62d82d0d7242aa15fa8bffc62b9b91214d3ef
John Negoita <johnny.php@gmail.com>
2016-10-22 09:26:27 +0300

    added SiteLog to store site check results

---
+++ api/controllers/SiteLogController.js
@@ -0,0 +1,11 @@
+/**
+ * SiteLogController
+ *
+ * @description :: Server-side logic for managing Sitelogs
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
--- api/models/SiteLog.js
+++ api/models/SiteLog.js
@@ -1,5 +1,5 @@
 /**
- * Site.js
+ * SiteLog.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
@@ -8,31 +8,18 @@
 module.exports = {
 
   attributes: {
-  	url:{
-  		type:'string'
-  	},
-  	lastCheck:{
-  		type:'datetime',
-  		defaultsTo:function(){
-  			return new Date();
-  		}
-  	},
-  	lastStatusCode:{
+  	statusCode:{
   		type:'integer'
   	},
-  	lastStatusMessage:{
+  	statusMessage:{
   		type:'string'
   	},
-  	avgResponseTime:{
+  	responseTime:{
   		type:'integer'
   	},
-    alertEmails:{
-      type:'array'
-    },
-
   	owner:{
-  		model:'userGroup'
-  	}
-  }  
+  		model:'Site'
+  	}  	
+  }
 };
 
