253350754d50978d099ccc88bacc991571c23370
John Negoita <johnny.php@gmail.com>
2016-11-06 17:14:00 +0200

    production server URL in ConfigService

--- api/services/ConfigService.js
+++ api/services/ConfigService.js
@@ -1,6 +1,7 @@
 module.exports = {
 	getServerURL:function(req){
 		// return "http://localhost:1337";
-		return "http://10.0.0.104:1337";
+		// return "http://10.0.0.104:1337";
+		return "http://www.upnow.info";
 	}
 }
